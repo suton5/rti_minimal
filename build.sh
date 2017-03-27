19ad430f51531cb4809491765149909a7c60c2be
Juan Jimeno <jimenojmm@gmail.com>
2017-03-27 18:00:37 +0800

    added xml file - works with nodejs

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 #source /home/juan/rti_connext_dds-5.2.3/resource/scripts/rtisetenv_x64Linux3gcc4.8.2.bash
+rm -rf src/objs
+rm src/*.h
+rm src/*.cxx
+rm src/*.xml
+rm src/make*
+
 cd src
 rtiddsgen -replace -language C++ -example x64Linux3gcc4.8.2 ../idl/*.idl -d ./
-make -f make*
+#rtiddsgen -replace -language C++ -example x64Linux3gcc4.8.2 -inputXml ../idl/*.xml -d ./
+
+#rtiddsgen -replace -language C++ -example armv6vfphLinux3.xgcc4.7.2 ../idl/*.idl -d ./
+
+make -f  app_makefile
 
---
+++ compile.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+rm -rf src/objs
+cd src
+make -f app_makefile

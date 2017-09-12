f7f6c8672c0097045d440d156a8a46fc1a5d8ad2
suton5 <suton5@gmail.com>
2017-09-12 10:51:38 +0800

    Fix issue with architecture

--- buildMac.sh
+++ buildMac.sh
@@ -9,9 +9,3 @@ rm src/make*
 #Generate sample codes for RTI pub-sub
 cd src
 ($NDDSHOME)/bin/rtiddsgen -replace -language C++ -example x64Darwin15clang7.0  ../idl/*.idl -d ./
-
-#Add libraries from app_makefile to generated makefile (this is a hack for now)
-perl -pi -e 's/INCLUDES =/INCLUDES +=/g' makefile*
-perl -pi -e 's/COMMONSOURCES =/COMMONSOURCES +=/g' makefile*
-
-make -f  app_makefile
